8297e3255fd158d934d8c9d9299de268ece7a5cd
Rostislav <rostwik@gmail.com>
2023-06-23 20:21:55 +0300

    Add products to menu

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
+from pprint import pprint
 
 import requests
 from flask import Flask, request
 from dotenv import load_dotenv
 
+from moltin import get_moltin_token, get_products, get_product_image, get_price
+
 app = Flask(__name__)
 load_dotenv()
-FACEBOOK_TOKEN = os.environ["FACEBOOK_TOKEN"]
+FACEBOOK_TOKEN = os.getenv('FACEBOOK_TOKEN')
+client_id = os.getenv('MOLTIN_CLIENT_KEY')
+client_secret = os.getenv('SECRET_KEY')
 
 
 @app.route('/', methods=['GET'])
@@ -16,7 +21,6 @@ def verify():
     """
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            print(request.args.get("hub.verify_token"), os.environ["VERIFY_TOKEN"])
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
@@ -35,27 +39,53 @@ def webhook():
                 if messaging_event.get("message"):
                     sender_id = messaging_event["sender"]["id"]
                     recipient_id = messaging_event["recipient"]["id"]
-                    message_text = messaging_event["message"]["text"]
-                    send_message(sender_id, message_text)
+                    moltin_token = get_moltin_token(client_id, client_secret)
+                    products = get_products(moltin_token)
+                    menu_items = []
+                    for product in products:
+                        price = get_price(moltin_token, product['id'])['RUB']['amount']
+                        menu_items.append(
+                            {'title': f"{product['attributes']['name']} {price}р.",
+                             'image_url': get_product_image(moltin_token, product['id']),
+                             'subtitle': product['attributes']['description'],
+                             'buttons': [{'type': 'postback', 'title': 'Добавить в корзину',
+                                          'payload': 'DEVELOPER_DEFINED_PAYLOAD'}]
+                             }
+                        )
+
+                    message = {
+                        "attachment": {
+                            "type": "template",
+                            "payload": {
+                                "template_type": "generic",
+                                "elements": menu_items[:5]
+
+                            }
+                        }
+                    }
+                    send_message(sender_id, message)
+
     return "ok", 200
 
 
-def send_message(recipient_id, message_text):
+def send_message(recipient_id, message):
     params = {"access_token": FACEBOOK_TOKEN}
     headers = {"Content-Type": "application/json"}
     request_content = {
         "recipient": {
             "id": recipient_id
         },
-        "message": {
-            "text": message_text
-        }
+        "message": message
     }
     response = requests.post(
         "https://graph.facebook.com/v2.6/me/messages",
         params=params, headers=headers, json=request_content
     )
     response.raise_for_status()
+
+
+def send_menu(recipient_id, message_text):
+    pass
 
 
 if __name__ == '__main__':
